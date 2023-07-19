b758edd5b9f3cf64aa0eccb8c6adf5e08aecaf88
mnhock <martin.hock.de@gmail.com>
2023-07-19 11:35:28 +0200

    Spring Data Properties spring.data.web.* are not evaluated
    Closes gh-14

--- naikan-web/src/main/java/com/enofex/naikan/NaikanApplication.java
+++ naikan-web/src/main/java/com/enofex/naikan/NaikanApplication.java
@@ -3,10 +3,8 @@ package com.enofex.naikan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
-@EnableSpringDataWebSupport
 @EnableConfigurationProperties(NaikanProperties.class)
 public class NaikanApplication {
 
