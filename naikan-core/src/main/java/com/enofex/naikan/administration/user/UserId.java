442f3fed68da82f9ddce5a80d62629214939c0e4
mnhock <Martin.Hock@gfk.com>
2023-07-28 16:50:05 +0200

    Clean up

--- naikan-core/src/main/java/com/enofex/naikan/administration/user/UserId.java
+++ naikan-core/src/main/java/com/enofex/naikan/administration/user/UserId.java
@@ -1,16 +1,6 @@
 package com.enofex.naikan.administration.user;
 
-public final class UserId {
-
-  private final String id;
-
-  UserId(String id) {
-    this.id = id;
-  }
-
-  public String id() {
-    return this.id;
-  }
+public record UserId(String id) {
 
   public static UserId of(String id) {
     return new UserId(id);
