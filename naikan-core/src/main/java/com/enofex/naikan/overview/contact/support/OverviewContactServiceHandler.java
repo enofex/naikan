6edfd8c0bfafa27205cf8d370ba5733a3d9449c2
mnhock <martin.hock.de@gmail.com>
2023-08-22 09:49:28 +0200

    Mark services as transactional read only

--- naikan-core/src/main/java/com/enofex/naikan/overview/contact/support/OverviewContactServiceHandler.java
+++ naikan-core/src/main/java/com/enofex/naikan/overview/contact/support/OverviewContactServiceHandler.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 class OverviewContactServiceHandler implements OverviewContactService {
 
   private final OverviewContactRepository overviewContactRepository;
--- naikan-core/src/main/java/com/enofex/naikan/overview/deployment/support/OverviewDeploymentServiceHandler.java
+++ naikan-core/src/main/java/com/enofex/naikan/overview/deployment/support/OverviewDeploymentServiceHandler.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 class OverviewDeploymentServiceHandler implements OverviewDeploymentService {
 
   private final OverviewDeploymentRepository overviewDeploymentRepository;
