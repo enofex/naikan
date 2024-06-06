f626f98ee0edf90662499b3f252bce94ed4a5a06
mnhock <martin.hock.de@gmail.com>
2024-06-06 21:08:28 +0200

    Use Taikai for architecture tests

--- naikan-server/src/test/java/com/enofex/naikan/architecture/ArchitectureTest.java
+++ naikan-server/src/test/java/com/enofex/naikan/architecture/ArchitectureTest.java
@@ -1,28 +1,43 @@
 package com.enofex.naikan.architecture;
 
-import static com.enofex.naikan.architecture.ConfigurationRules.configurationRules;
-import static com.enofex.naikan.architecture.ControllerRules.controllerRules;
-import static com.enofex.naikan.architecture.RepositoryRules.repositoryRules;
-import static com.enofex.naikan.architecture.ServiceRules.serviceRules;
-import static com.enofex.naikan.architecture.SpringRules.springBootRules;
-
-import com.enofex.naikan.test.architecture.ArchUnitTestsConfig;
-import java.util.Collection;
-import org.junit.jupiter.api.DynamicTest;
-import org.junit.jupiter.api.TestFactory;
+import com.enofex.taikai.Taikai;
+import org.junit.jupiter.api.Test;
 
 class ArchitectureTest {
 
-  @TestFactory
-  Collection<DynamicTest> shouldFulfilArchitectureConstrains() {
-    ArchUnitTestsConfig config = ArchUnitTestsConfig.defaultConfig();
+  @Test
+  void shouldFulfilConstrains() {
+    Taikai taikai = Taikai.builder()
+        .namespace("com.enofex.naikan")
+        .spring(spring -> spring
+            .noAutowiredFields()
+            .boot(boot -> boot
+                .springBootApplicationShouldBeIn("com.enofex.naikan"))
+            .controllers(controllers -> controllers
+                .shouldBeAnnotatedWithRestController()
+                .namesShouldEndWithController()
+                .shouldNotDependOnOtherControllers()
+                .shouldBePackagePrivate())
+            .services(services -> services
+                .shouldBeAnnotatedWithService()
+                .namesShouldEndWithService())
+            .repositories(repositories -> repositories
+                .namesShouldEndWithRepository()))
+        .test(test -> test
+            .junit5(junit5 -> junit5
+                .classesShouldNotBeAnnotatedWithDisabled()
+                .methodsShouldNotBeAnnotatedWithDisabled()))
+        .java(java -> java
+            .imports(imports -> imports
+                .shouldHaveNoCycles()
+                .shouldNotImport("..shaded..")
+                .shouldNotImport("..lombok..")
+                .shouldNotImport("org.junit.."))
+            .naming(naming -> naming
+                .classesShouldNotMatch(".*Impl")
+                .interfacesShouldNotHavePrefixI()))
+        .build();
 
-    config.addDynamicTests(springBootRules(config));
-    config.addDynamicTests(controllerRules(config));
-    config.addDynamicTests(serviceRules(config));
-    config.addDynamicTests(repositoryRules(config));
-    config.addDynamicTests(configurationRules(config));
-
-    return config.getDynamicTests();
+    taikai.check();
   }
 }
